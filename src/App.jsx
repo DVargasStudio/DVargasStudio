d4df300e28e84691b932f3db8be1010e6e536fe8
grramp <grampelottorodrigues@gmail.com>
2022-02-13 19:49:05 -0300

    HomePage Cover created -- in progress

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./global.scss"
+import Cover from "./components/cover/Cover"
+
+function App() {
+  return (
+    <div>
+      <Cover/>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/components/cover/Cover.jsx
@@ -0,0 +1,14 @@
+import "./cover.scss"
+
+export default function Cover() {
+  return (
+    <div class="Cover">
+      <div class="Cover-Title">
+         <h2>Traga sua ideia</h2>
+         <h1>Nós tiramos do papel</h1>
+      </div>
+      <div class="Space">
+      </div>
+    </div>
+  )
+}
