d030dc1fb4930d887b8eb6453403fc02d26ac6b8
grramp <grampelottorodrigues@gmail.com>
2022-03-14 15:35:55 -0300

    video added and covers changed

--- src/pages/PortfolioPages/DragDeal.jsx
+++ src/pages/PortfolioPages/DragDeal.jsx
@@ -1,8 +1,11 @@
 import './portfolioPages.scss';
 
+import ReactPlayer from "react-player";
 import {Helmet} from 'react-helmet-async';
 
 import Header from "./../../components/header/Header"
+
+import video from './../../assets/videos/DragDeal.mp4'
 
 import imgCover from './../../assets/imgs/DragDealCover.png'
 import unityLogo from './../../assets/imgs/UnityLogo.png'
@@ -40,7 +43,8 @@ export default function DragDeal({refHeader}) {
           <p>As ferramentas que estão sendo utilizadas para esse projeto foram o Unity como game engine, o Character Creator para a criação das personagens, o Maya para a modelagem dos assets e o Reaper para a mixagem e edição sonora.</p>
         </div>
 
-      <h2>Em desenvolvimento</h2>
+      <h2>Em Breve</h2>
+      <ReactPlayer url={video} width={"calc(100px + 35%)"} height="35%" pip={true} controls={true} className="PortfolioPages-Video"/>
       <p className='Center'>Para saber mais, clique no botão abaixo</p>
       <a href="https://www.instagram.com/dragdeal_game/" target="_blank" rel="noreferrer"><h4 className='PortfolioPages-Button'>Ver Mais</h4></a>
     </div>
